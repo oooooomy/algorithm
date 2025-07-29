285abb2bdcc2bd3248ccb69744235f661f27da69
Yuanming Gao <51779749+oooooomy@users.noreply.github.com>
2025-07-29 22:32:18 +0800

    Delete .idea directory

---
+++ src/list/Partition.java
@@ -0,0 +1,61 @@
+package org.oooooomy.list;
+
+/**
+ * 86. 分隔链表
+ */
+public class Partition {
+
+    public static void main(String[] args) {
+        ListNode node = ListNode.create(new int[]{1, 4, 3, 2, 5, 2});
+        node.print();
+        ListNode partition = partition(node, 3);
+        partition.print();
+    }
+
+    /**
+     * 错误做法
+     */
+//    public static ListNode partition(ListNode head, int x) {
+//        ListNode p = head;
+//        ListNode dummy1 = new ListNode();
+//        ListNode dummy2 = new ListNode();
+//        ListNode h1 = dummy1;
+//        ListNode h2 = dummy2;
+//        while (p != null) {
+//            if (p.val < x) {
+//                h1.next = p; //这里的p没有和原节点断开，最后会形成环
+//                h1 = h1.next;
+//            } else {
+//                h2.next = p;
+//                h2 = h2.next;
+//            }
+//            p = p.next;
+//        }
+//        h1.next = dummy2.next;
+//        return dummy1.next;
+//    }
+
+    public static ListNode partition(ListNode head, int x) {
+        ListNode p = head;
+        ListNode dummy1 = new ListNode();
+        ListNode dummy2 = new ListNode();
+        ListNode h1 = dummy1;
+        ListNode h2 = dummy2;
+        ListNode next;
+        while (p != null) {
+            next = p.next;
+            p.next = null;
+            if (p.val < x) {
+                h1.next = p;
+                h1 = h1.next;
+            } else {
+                h2.next = p;
+                h2 = h2.next;
+            }
+            p = next;
+        }
+        h1.next = dummy2.next;
+        return dummy1.next;
+    }
+
+}
