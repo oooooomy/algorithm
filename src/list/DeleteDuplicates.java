569ed9dc4c78e9c5d95cdbd2f718e955dbdd5bb2
000000my <1729677089@qq.com>
2025-08-02 13:30:06 +0800

    83. 删除排序链表中的重复元素

---
+++ src/list/DeleteDuplicates.java
@@ -0,0 +1,30 @@
+package org.oooooomy.list;
+
+/**
+ * 83. 删除排序链表中的重复元素
+ */
+public class DeleteDuplicates {
+
+    public static void main(String[] args) {
+//        ListNode node = ListNode.create(new int[]{1, 1, 2, 3, 3});
+        ListNode node = ListNode.create(new int[]{1, 1, 2});
+        ListNode h = new DeleteDuplicates().deleteDuplicates(node);
+        h.print();
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = head;
+        while (p.next != null) {
+            if (p.val == p.next.val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+        return head;
+    }
+
+}
