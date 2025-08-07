d7ada42d1ca9c86c5e39ecd433957dc94642a60d
oooooomy <1729677089@qq.com>
2025-08-07 16:54:06 +0800

    136. 只出现一次的数字

---
+++ src/skill/SingleNumber.java
@@ -0,0 +1,39 @@
+package org.oooooomy.skill;
+
+import java.util.*;
+
+/**
+ * 136. 只出现一次的数字
+ */
+public class SingleNumber {
+
+    //最优解
+    public static int singleNumber(int[] nums) {
+        int ans = nums[0];
+        if (nums.length > 1) {
+            for (int i = 1; i < nums.length; i++) {
+                ans = ans ^ nums[i];
+            }
+        }
+        return ans;
+    }
+
+
+    public static int singleNumber2(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            }else  {
+                map.put(i, 1);
+            }
+        }
+        for (Integer key : map.keySet()) {
+            if (map.get(key) == 1) {
+                return key;
+            }
+        }
+        return 0;
+    }
+
+}
