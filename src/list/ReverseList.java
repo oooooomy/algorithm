5528354fce93cfeab1edf50172c5e4ddbf810fbe
oooooomy <1729677089@qq.com>
2025-08-12 23:32:26 +0800

    206. 反转链表

--- src/list/ReverseList.java
+++ src/list/ReverseList.java
@@ -1,9 +1,20 @@
 package org.oooooomy.list;
 
+/**
+ * 206. 反转链表
+ */
 public class ReverseList {
 
     public ListNode reverseList(ListNode head) {
-
+        ListNode cur = head;
+        ListNode pre = null;
+        while (cur != null) {
+            ListNode n = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = n;
+        }
+        return pre;
     }
 
 }
