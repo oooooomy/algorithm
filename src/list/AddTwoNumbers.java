4b312c13a704e385bf899d02ef1dfed0f0b350b5
oooooomy <1729677089@qq.com>
2025-08-16 18:34:26 +0800

    2. 两数相加

---
+++ src/list/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+package org.oooooomy.list;
+
+/**
+ * 2. 两数相加
+ */
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    }
+
+}
--- src/list/DetectCycle.java
+++ src/list/DetectCycle.java
@@ -1,11 +1,39 @@
 package org.oooooomy.list;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 142. 环形链表 II
  */
 public class DetectCycle {
 
     public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            //快慢节点相遇
+            if (slow == fast) {
+                while (slow != head) {
+                    slow = slow.next;
+                    head = head.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+
+    public ListNode detectCycle2(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return head;
+            }
+            set.add(head);
+            head = head.next;
+        }
         return null;
     }
 
